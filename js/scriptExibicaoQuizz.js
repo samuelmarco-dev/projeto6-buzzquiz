96f9f6dafc405225c4054f1cfe9692d6f782cd9c
Guilherme Alves <guialvest@gmail.com>
2022-02-19 16:56:26 -0300

    Implementação da página de exibição do quizz

---
+++ js/scriptExibicaoQuizz.js
@@ -0,0 +1,162 @@
+const homeQuizz= document.querySelector(".home-quizz");
+const exibicaoQuizz = document.querySelector(".exibicao-quizz");
+const criandoQuizz = document.querySelector(".criacao-de-quizz");
+const exibicaoPerguntas = document.querySelector(".exibicao-perguntas");
+let grupoDeRespostas, arrayDeRespostas=[];
+let idSelecionado, titulo,urlImagem, objetoPerguntas=[],objetoLeveis=[],acertos=0,porcentagemAcertos;
+
+
+//abrindo o quizz na página 2 pelo seu id
+function abrindoQuizz(quizzId){
+    homeQuizz.classList.add("escondido");
+    exibicaoQuizz.classList.remove("escondido");
+    exibicaoQuizz.scrollIntoView();
+    idSelecionado = parseInt(quizzId);
+    identificandoQuizz();
+}
+//identificando qual quizz deverá ser aberto
+function identificandoQuizz(){
+    for(let i=0; i<objetoQuizzes.length;i++){
+        if(objetoQuizzes[i].id===idSelecionado){
+            titulo=objetoQuizzes[i].title;
+            urlImagem=objetoQuizzes[i].image;
+            objetoPerguntas = objetoQuizzes[i].questions;
+            objetoLeveis=objetoQuizzes[i].levels;
+            continue;
+        }
+    }
+    colocarImagemNoTopo();
+    renderizandoPerguntas();
+}
+//colocando imagem e título no topo
+function colocarImagemNoTopo(){
+    const imagemTopo = document.querySelector(".imagem-topo");
+    //definindo tamanho da imagem
+    imagemTopo.style.background = `linear-gradient(0deg, rgba(0, 0, 0, 0.57), rgba(0, 0, 0, 0.57)), url(${urlImagem})`;
+    imagemTopo.style.backgroundSize = "cover";
+    imagemTopo.style.backgroundPosition = "center center";
+
+    //colocando o título sobre a imagem
+    imagemTopo.innerHTML=`
+    <h1>${titulo}</h1>
+    `;
+}
+//renderizando as perguntas na tela
+function renderizandoPerguntas(){
+    objetoPerguntas.forEach(pergunta=>{
+        exibicaoPerguntas.innerHTML+=`
+        <div class="exibicao-pergunta questao${objetoPerguntas.indexOf(pergunta)}">
+            <div class="titulo-pergunta">
+                <p>${pergunta.title}</p>
+            </div>
+            <div class="respostas-pergunta">
+            
+            </div>
+        </div>
+        `
+        //embaralhando array de respostas
+        let respostasEmbaralhadas = pergunta.answers;
+        respostasEmbaralhadas.sort(embaralhandoArray);
+        //chamando metodos de renderização da resposta / cor do título
+        renderizandoResposta(respostasEmbaralhadas,objetoPerguntas.indexOf(pergunta));
+        definindoCorDoTitulo(pergunta.color,objetoPerguntas.indexOf(pergunta));
+    });
+}
+//aqui se encontra a função para embaralhar uma array
+function embaralhandoArray(){
+    return Math.random() - 0.5; 
+}
+//definindo a cor de fundo do título
+function definindoCorDoTitulo(corDoTitulo,indiceDaPergunta){
+    const areaDoTitulo = [...document.querySelectorAll(".titulo-pergunta")];
+    areaDoTitulo[indiceDaPergunta].style.backgroundColor=`${corDoTitulo}`;
+}
+//renderizando as respostas contidas em cada pergunta
+function renderizandoResposta(respostas,indiceDaPergunta){
+    grupoDeRespostas = [...document.querySelectorAll('.respostas-pergunta')];
+    for(let i=0; i<respostas.length; i++){
+        //dentro de cada "grupo de respostas", terá várias divs, cada uma contendo uma resposta
+        grupoDeRespostas[indiceDaPergunta].innerHTML+=`
+        <div class="resposta-pergunta pergunta${indiceDaPergunta} resposta${respostas[i].isCorrectAnswer}" onclick="escolherResposta(this,${indiceDaPergunta})">
+
+           <img src="${respostas[i].image}" alt="Imagem que representa uma resposta">
+           <p>${respostas[i].text}</p>
+
+        </div>
+        `;
+    }
+}
+//definindo algoritmo para a escolha de uma resposta
+function escolherResposta(respostaSelecionada,indiceDaPergunta){
+    mudandoCorDoTexto(indiceDaPergunta);
+    diminuirOpacidadeDasRespostas(indiceDaPergunta);
+    respostaSelecionada.classList.remove("diminuir-opacidade");
+    if(respostaSelecionada.classList.contains("respostatrue")){
+        acertos+=1;
+    }
+    scrollarParaProximaPergunta(indiceDaPergunta);
+}
+//mudando a cor do texto das respostas certas/erradas
+function mudandoCorDoTexto(indiceDaPergunta){
+    const textosDasRespostas=[...document.querySelectorAll(`.pergunta${indiceDaPergunta} p`)];
+    //colocando cores nos textos da resposta certa/errada
+    textosDasRespostas.forEach(texto=>{
+        if(texto.parentNode.classList.contains("respostatrue")){
+            texto.style.color="#009C22";
+        }
+        else{
+            texto.style.color="#FF4B4B";
+        }
+    })
+}
+//diminuindo a opacidade das respostas não escolhidas
+function diminuirOpacidadeDasRespostas(indiceDaPergunta){
+    const respostasDaPergunta=[...document.querySelectorAll(`.pergunta${indiceDaPergunta}`)];
+    //diminuindo opacidade das respostas não escolhidas
+    respostasDaPergunta.forEach(respostaDaPergunta=>{
+        respostaDaPergunta.classList.add("diminuir-opacidade");
+        respostaDaPergunta.removeAttribute("onclick");
+    })
+}
+//escrolando automaticamente e identificando a % de acertos
+function scrollarParaProximaPergunta(indiceDaPergunta){
+    const indiceProximaPergunta = parseInt(indiceDaPergunta)+1;
+    const proximaPergunta=document.querySelector(`.questao${indiceProximaPergunta}`);
+    if(proximaPergunta!==null){
+        setTimeout(()=>{
+            proximaPergunta.scrollIntoView({block: "center", behavior:"smooth"});
+        },2000);
+    }
+    else if(proximaPergunta===null){
+        const resultadoDoQuizz=document.querySelector(".resultado-do-quizz");
+        setTimeout(()=>{
+            resultadoDoQuizz.classList.remove("escondido");
+            resultadoDoQuizz.scrollIntoView({block: "center", behavior:"smooth"});
+        },2000);
+        const quantidadeDeQuestões = objetoPerguntas.length;
+        porcentagemAcertos = Math.round(acertos*100/quantidadeDeQuestões);
+        construindoResultado(resultadoDoQuizz);
+    }
+}
+//indentificando o level do resultado e mostrando na tela
+function construindoResultado(AbaDeResultado){
+    let tituloResultado=AbaDeResultado.querySelector(".titulo-do-resultado");
+    for(let i = objetoLeveis.length-1; i>=0;i--){
+        const tituloLevel = objetoLeveis[i].title;
+        const imagemLevel = objetoLeveis[i].image;
+        const textoLevel = objetoLeveis[i].text;
+        const porcentagemMinima = objetoLeveis[i].minValue;
+        if(porcentagemAcertos>=porcentagemMinima){
+            console.log(porcentagemAcertos);
+            console.log(porcentagemMinima);
+            tituloResultado.innerHTML=`<p>Você acertou ${porcentagemAcertos}%:
+            ${tituloLevel}</p>
+            `;
+            AbaDeResultado.innerHTML+=`
+            <img src="${imagemLevel}" alt="imagem do lovel">
+            <p><span>${textoLevel}</span></p>
+            `;
+            break;
+        }
+    }
+}
--- js/scriptHomeQuizz.js
+++ js/scriptHomeQuizz.js
@@ -1,32 +1,57 @@
 const api = "https://mock-api.driven.com.br/api/v4/buzzquizz/";
-let allQuizzesListHtml = document.querySelector(".all-quizzes .quizzesList");
-let yourQuizzesListHtml = document.querySelector(".your-quizzes .quizzesList");
+const listaQuizzesLocais =document.querySelector(".seus-quizzes .quizzes-lista");
+let objetoQuizzes, urlImagens=[];
+let listaTodosOsQuizzes = document.querySelector(".todos-quizzes .quizzesLista");
+let listaSeusQuizzes = document.querySelector(".seus-quizzes .quizzesLista");
 
 //fazendo requisição para obter o objeto contendo os quizzes
-function gettingAllQuizzes(){
-    const quizzesRequested = axios.get(`${api}quizzes`);
-    quizzesRequested.then(result =>{
-        const quizzesObjects = result.data;
-        console.log(quizzesObjects);
-        renderQuizzes(quizzesObjects);
+function pegandoTodosOsQuizzes(){
+    const requisicaoDosQuizzes = axios.get(`${api}quizzes`);
+    requisicaoDosQuizzes.then(resultado =>{
+        objetoQuizzes = resultado.data;
+        renderQuizzes(objetoQuizzes);
     })
 }
 
 //renderizando os quizzes na Home-quizz
-function renderQuizzes(quizzesObjects){
-    quizzesObjects.forEach(quizzObject => {
-
-        const title = quizzObject.title;
-        const image = quizzObject.image;
-        allQuizzesListHtml.innerHTML+=`
-            <article class="quizz">
-                    <p>${title}</p>
-                    <img src= ${image} alt="Imagem do quizz">
+function renderQuizzes(objetoQuizzes){
+    objetoQuizzes.forEach(objetoquizz => {
+        urlImagens.push(objetoquizz.image);
+        const titulo = objetoquizz.title;
+        const id = objetoquizz.id
+        //renderizando cada um dos quizzes
+        listaTodosOsQuizzes.innerHTML+=`
+            <article onclick="abrindoQuizz('${id}')" class="quizz">
+                <p>${titulo}</p>
             </article>
         `;
-
     });
+    let areaDoQuiz = [...document.querySelectorAll(".quizz")];
+    colocandoImagemNoQuizz(areaDoQuiz);
 }
-
+//verificando a existência de quizzes locais
+function conferindoQuizzesLocais(){
+    if(listaQuizzesLocais.innerHTML===""){
+        listaQuizzesLocais.parentNode.innerHTML=`
+            <article class="sem-quizz">
+                <p>Você não criou nenhum <br> quizz ainda :(</p>
+                <button onclick="criarQuizz()" class="criar-quizz">Criar Quizz</button>
+            </article>
+        `;
+    }
+}
+//colocando imagem no quizz pelo backgroundo do css
+function colocandoImagemNoQuizz(areaDoQuiz){
+    for(let i=0; i<areaDoQuiz.length; i++){
+        areaDoQuiz[i].style.background = `linear-gradient(180deg, rgba(255, 255, 255, 0) 30%, rgba(0, 0, 0, 0.5) 65%, #000000 100%), url(${urlImagens[i]})`;
+        areaDoQuiz[i].style.backgroundSize = "340px 181px";
+    }
+}
+//criando primeiro quizz
+function criarQuizz(){
+    homeQuizz.classList.add("escondido");
+    criandoQuizz.classList.remove("escondido");
+}
 //chamando funções
-gettingAllQuizzes();
+pegandoTodosOsQuizzes();
+conferindoQuizzesLocais();
